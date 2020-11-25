b6b7d6569750c692dbf1f514b9ded328774bf9bb
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-11-25 22:28:06 +0530

    raphael-sepolicy: Initial raphael sepolicy
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

---
+++ raphael-sepolicy.mk
@@ -0,0 +1,6 @@
+PRODUCT_PUBLIC_SEPOLICY_DIRS += device/xiaomi/raphael-sepolicy/public
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/xiaomi/raphael-sepolicy/private
+
+# Vendor
+BOARD_SEPOLICY_DIRS += device/xiaomi/raphael-sepolicy/vendor/xiaomi
+BOARD_SEPOLICY_DIRS += device/xiaomi/raphael-sepolicy/vendor/qcom/sm8150
